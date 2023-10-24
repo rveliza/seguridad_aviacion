e15d859b26d84aa38abfc0a0ea69ae6d8417720e
Reyner Veliz <reyner@reynerveliz.com>
2023-10-24 05:22:12 -0600

    Added Inicio for multipage app

---
+++ Inicio.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.header("Seguridad Aviación Corporativa")
+
+st.subheader("Objetivos")
+
+st.write("Elevar la seguridad de la operación de los vuelos en Jet a través de un conocimiento básico de algunos sistemas de la aeronave, performance, peso y balance, regulaciones, y procedimientos de emergencia.")
+
--- main.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-
-st.header("Seguridad Aviacion Corporativa")
-
