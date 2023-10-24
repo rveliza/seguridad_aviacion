ca920635eaa20a3f5059fb4ebbd577574dd692bd
Reyner Veliz <reyner@reynerveliz.com>
2023-10-24 07:42:43 -0600

    Add avion general sketch

--- Inicio.py
+++ Inicio.py
@@ -5,5 +5,13 @@ st.header("Seguridad Aviación Corporativa")
 
 st.subheader("Objetivos")
 
-st.write("Elevar la seguridad de la operación de los vuelos en Jet a través de un conocimiento básico de algunos sistemas de la aeronave, performance, peso y balance, regulaciones, y procedimientos de emergencia.")
+st.markdown("""
+- Elevar la seguridad en la operación de los vuelos en jet a través de un conocimiento básico de:
+    - sistemas de la aeronave 
+    - performance 
+    - peso y balance 
+    - regulaciones
+    - procedimientos de emergencia.
+
+""")
 
---
+++ pages/01_✈️_Avión_General.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.markdown("""
+- Luces de cabina
+- Señal de cinturón
+    - Importancia y significado
+    - Ejemplos de procedimientos en los manuales que hacen referencia a su uso.
+
+- Enchufes eléctricos
+    - Por qué se desconectan durante fases de vuelo
+
+- Puertas del avión
+    - Puerta Principal
+        - Uso adecuado
+        - Peligros asociados 
+             - Vent Flap
+             - Cable
+             - Peso de puerta
+            
+    - Salida de Emergencia
+        - Operación
+        - Hacer una práctica
+- Baño
+    - Cantidad de líquido
+    - Relación con sistema de presurización
+    - Puertas del baño. (Operación adecuada)
+            
+- Equipo de Emergencia
+    - Extinguidores de Fuego
+    - Primeros Auxilios
+    - Mascarillas de Oxígeno
+    - Balsa y chalecos
+    - Portable Breathing Equipment (PBE)
+    - Water Barrier
+""")
